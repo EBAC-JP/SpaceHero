0649dedb7e9289df8f08eb6277f2522d44ae1932
João Paulo Busche <joaopaulo.busche@gmail.com>
2022-04-04 21:50:50 -0300

    Make Player Jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,8 +6,12 @@ public class Player : MonoBehaviour {
 
     [Header("Moviments")]
     [SerializeField] float speed;
+    [SerializeField] Vector2 friction = new Vector2(-.1f, 0);
     [SerializeField] KeyCode leftMoviment;
     [SerializeField] KeyCode rigthMoviment;
+    [Header("Jump")]
+    [SerializeField] float jumpForce;
+    [SerializeField] KeyCode jumpKey;
 
     Rigidbody2D  _myRigid;
 
@@ -16,10 +20,27 @@ public class Player : MonoBehaviour {
     }
 
     void Update() {
+        HandleJump();
+        HandleMoviment();
+    }
+
+    void HandleJump() {
+        if (Input.GetKeyDown(jumpKey)) {
+            _myRigid.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void HandleMoviment() {
         if (Input.GetKey(rigthMoviment)) {
             _myRigid.velocity = new Vector2(speed, _myRigid.velocity.y);
         } else if(Input.GetKey(leftMoviment)) {
             _myRigid.velocity = new Vector2(-speed, _myRigid.velocity.y);
         }
+        HandleFriction();
+    }
+
+    void HandleFriction() {
+        if (_myRigid.velocity.x > 0) _myRigid.velocity += friction;
+        else if (_myRigid.velocity.x < 0) _myRigid.velocity -= friction;
     }
 }
